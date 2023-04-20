59a5f3bcb684d9d5bd6cdb81484f38729b61f5f6
pabloggarc <pablogg.ext.88.22@gmail.com>
2023-04-20 21:30:52 +0200

    Prolog a LaTeX completo

--- Prolog2LaTeX.py
+++ Prolog2LaTeX.py
@@ -1,13 +1,45 @@
+import os, shutil
+from datetime import datetime
+from pdf2image import convert_from_path
+
+
 keyWords = {"oc": "\\textbf{OC}", "os": "\\textbf{OS}", "gv": "\\textbf{GV}", "gn": "\\textbf{GN}"}
 
 def parser(sol, keyWords): 
-    tex_header = "\\documentclass[12pt]{article}\n\\usepackage{qtree}\n\\begin{document}\n\t\\Tree "
-    tex = "[." + sol.replace("(", " [.").replace(", ", "] [.").replace(")", " ] ") + " ]\n" 
+    tex_header = "\\documentclass{standalone}\n\\usepackage{qtree}\n\\begin{document}\n\t\\Tree "
+    tex_body = "[." + sol.replace("(", " [.").replace(", ", "] [.").replace(")", " ] ") + " ]\n" 
     tex_foot = "\\end{document}"
 
     for word in keyWords.keys(): 
-        tex = tex.replace(word, keyWords[word])
+        tex_body = tex_body.replace(word, keyWords[word])
 
-    return (tex_header + tex + tex_foot).replace("'", "")
+    file_name = datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".tex"
 
-print(parser("oc(os(gn(np('HÉCTOR')), gv(v(come), gn(n(patatas), adj(fritas)))), conj(y), os(gv(v(bebe), gn(n(zumo)))))", keyWords))
+    with open(file_name, "w", encoding='utf-8') as tex: 
+        tex.write((tex_header + tex_body + tex_foot).replace("'", ""))
+
+    return file_name
+
+def compile(file): 
+    temp = [".tex", ".aux", ".log"]
+    os.system("pdflatex " + file + " > NUL 2>&1")
+    for temp_file in temp: 
+        os.system("del *" + temp_file + " > NUL 2>&1")
+
+def toJPG(file): 
+    pdf = convert_from_path(file.replace(".tex", ".pdf"))
+    pdf[0].save(file.replace(".tex", ".jpg"), "JPEG")
+    os.system("del " + file.replace(".tex", ".pdf") + " > NUL 2>&1")
+
+def show(file, mode): 
+    if mode == "-pdf": 
+        os.startfile(file.replace(".tex", ".pdf"))
+    elif mode == "-img": 
+        toJPG(file)
+        os.startfile(file.replace(".tex", ".jpg"))
+
+def main(prolog_input, mode): 
+    p = parser(prolog_input, keyWords)
+    compile(p)
+    show(p, mode)
+    shutil.rmtree('__pycache__')
--- analizador.py
+++ analizador.py
@@ -1,17 +1,24 @@
-import sys
+import sys, os
 from pyswip import Prolog
+from Prolog2LaTeX import main
 
-frase = sys.argv[1]
-sub_query1 = "oracion(X, ["
-sub_query2 = "" 
-sub_query3 = "], []), draw(X). "
+if len(sys.argv) != 3: 
+    print("Se esperaban argumentos -pdf / -img 'frase'")
+    sys.exit(-1)
+else: 
+    frase = sys.argv[-1]
+    modo = sys.argv[1]
+    sub_query1 = "oracion(X, ["
+    sub_query2 = "" 
+    sub_query3 = "], []), draw(X). "
 
-for palabra in frase.split(" "): 
-    sub_query2 += "'" + palabra + "', "
+    for palabra in frase.split(" "): 
+        sub_query2 += "'" + palabra + "', "
 
-query = sub_query1 + sub_query2[: - 2] + sub_query3
+    query = sub_query1 + sub_query2[: - 2] + sub_query3
 
-prolog = Prolog()
-prolog.consult("gramatica.pl")
+    prolog = Prolog()
+    prolog.consult("gramatica.pl")
 
-for answer in prolog.query(query): pass
+    for answer in prolog.query(query):
+        main(answer['X'], sys.argv[1])
