ad88b0d0d0815ef3e427adf1bdb563f0a33757b5
alej1000 <alejandro.raboso@edu.uah.es>
2023-04-23 13:25:17 +0200

    Diccionario inglés, falta modificar las "cosas raras"

---
+++ diccionario_eng.pl
@@ -0,0 +1,95 @@
+%Diccionario de palabras en inglés
+
+% Names
+np('JOS\xC9', 'JOSE').
+np('MAR\xCD\A', 'MARIA').
+np('H\xC9\CTOR', 'HECTOR').
+np('IRENE', 'IRENE').
+np('Filosof\xED\a', 'Philosophy').
+np('Derecho', 'Law').
+
+% Nouns
+n('caf\xE9','coffee'). %café
+n('mesa','table').
+n('peri\xF3\dico','newspaper'). %periódico
+n('patatas','potatoes').
+n('paella','paella').
+n('cerveza','beer').
+n('zumo','juice').
+n('roc\xF3\dromo','climbing wall'). %rocódromo
+n('manzanas','apples').
+n('documentos','documents').
+n('rat\xF3\n','mouse'). %ratón
+n('gato','cat').
+n('hombre','man').
+n('vecino','neighbour'). %neighbor 
+n('novela','novel').
+n('herramienta','tool').
+n('tardes','afternoons').
+n('procesador','processor').
+n('textos','texts'). %word -> word processor/text processor
+
+% Verbs
+v('es','is'). %it's -> it's quite slow (frase 12)
+v('estudia','studies').
+v('toma','takes').
+v('recoge','picks up'). %clears
+v('lee','reads').
+v('comen','eat').
+v('beben','drink').
+v('prefiere','prefers').
+v('escala','climbs').
+v('come','eats').
+v('sirve','serves').
+v('caz\xF3','hunted'). %cazó
+v('vimos','saw'). % we saw -> que vimos: we saw (frase 14)
+v('canta','sings').
+v('salta','jumps').
+v('bebe','drinks').
+v('escribir','write').
+v('era','was').
+
+% Adjectives
+adj('moreno','brunette').
+adj('alta','tall').
+adj('gris','gray'). %grey
+adj('potente','powerful').
+adj('lento','slow').
+adj('\xE1\gil','agile'). %ágil
+adj('delicado','delicate'). %picky -> Lo usan para comida (frase 10) asiq quisquilloso?
+adj('fritas','fried').
+adj('rojas','red').
+
+
+% Determiners
+det('el','the').
+det('un','a').
+det('una','a').
+det('la','the').
+det('las','the').
+det('mi','my').
+det('El','The').
+
+% Conjunctions
+conj('y','and').
+conj('pero','but').
+conj('aunque','although').
+conj('e','and').
+conj('que','that'). %who -> Him who is something / %which -> The one which is something 
+conj('mientras','while').
+
+% Prepositions
+prep('en','in'). %at -> at the table
+prep('por','by'). %in -> in the evenings
+prep('de','of').
+prep('para','for'). %to -> to write documents 
+ 
+
+% Adverbs
+adv('muy','very').
+adv('solamente','only').
+adv('bastante','quite').
+adv('ayer','yesterday').
+
+% Pronouns
+pro('que','that'). %who -> Him who is something / %which -> The one which is something 
