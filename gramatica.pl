048c338df33500d7c4a77d7c58996bde3c744473
AlvaroMtzz <92862890+AlvaroMtzz@users.noreply.github.com>
2023-04-26 12:58:13 +0200

    Cambios menos en descomponer

--- gramatica.pl
+++ gramatica.pl
@@ -1,9 +1,10 @@
 :- ['diccionario_esp.pl']. 
 :- ['draw.pl'].
+:- ['traductor.pl'].
 :- ['descomponer.pl'].
 
 
-% Terminales
+%---SÍMBOLOS TERMINALES---
 nombre_propio(np(X)) --> [X],{np(X)}.
 nombre(n(X)) --> [X],{n(X)}.
 verbo(v(X)) --> [X],{v(X)}.
@@ -15,8 +16,7 @@ adverbio(adv(X)) --> [X],{adv(X)}.
 pronombre(pro(X)) --> [X],{pro(X)}.
 infinitivo(inf(X)) --> [X],{inf(X)}.
 
-% No terminales -> Sintagmas
-
+%---SINTAGMAS---
 % Sintagma nominal  
 grupo_nominal(gn(N)) --> nombre_propio(N).
 grupo_nominal(gn(NA,C,NB)) --> nombre_propio(NA), conjuncion(C), nombre_propio(NB).
@@ -27,19 +27,18 @@ grupo_nominal(gn(D,N,GA)) --> determinante(D), nombre(N), grupo_adjetival(GA).
 grupo_nominal(gn(D,N,GP)) --> determinante(D), nombre(N), grupo_preposicional(GP).
 grupo_nominal(gn(I,GN)) --> infinitivo(I), grupo_nominal(GN).
 
+% Sintagma nominal compuesto por una estructura de sintagma nominal y una oración subordinada de relativo(la cual también pertenece al SN en sí)
 grupo_nominal_compuesto(gn(GN, OSR)) --> grupo_nominal(GN), oracion_subordinada_rel(OSR).
-
 
 % Sintagma preposicional
 grupo_preposicional(gp(P,GN)) --> preposicion(P), grupo_nominal(GN).
 
-% Sintagma adjatival
+% Sintagma adjetival
 grupo_adjetival(gadj(A)) --> adjetivo(A).
 grupo_adjetival(gadj(AV,A)) --> adverbio(AV), adjetivo(A).
 grupo_adjetival(gadj(A,C,GA)) --> adjetivo(A), conjuncion(C), grupo_adjetival(GA).
 
 % Sintagma verbal
-
 grupo_verbal(gv(VA,C,VB)) --> verbo(VA), conjuncion(C), verbo(VB).
 grupo_verbal(gv(V,GN)) --> verbo(V), grupo_nominal(GN).
 grupo_verbal(gv(V,GN)) --> verbo(V), grupo_nominal_compuesto(GN).
@@ -52,42 +51,58 @@ grupo_verbal(gv(AV,GV)) --> adverbio(AV), grupo_verbal(GV).
 grupo_verbal(gv(V,GA,GN)) --> verbo(V), grupo_adverbial(GA), grupo_nominal(GN).
 grupo_verbal(gv(V)) --> verbo(V).
 
+% Sintagma verbal compuesto por una estructura de sintagma verbal y una oración subordinada de relativo(la cual también pertenece al SV en sí)
 grupo_verbal_compuesto(gv(GV, OSR)) --> grupo_verbal(GV), oracion_subordinada_rel(OSR).
-
 
 % Sintagma adverbial 
 grupo_adverbial(gadv(A)) --> adverbio(A).
 
-% Oraciones 
+
+%---ORACIONES---
+% Una oración puede ser una oración simple o una oración compuesta 
 oracion(X, L, []):- oracion_simple(X, L, []), !.
 oracion(X, L, []):- oracion_compuesta(X, L, []), !.
 
-
+% Una oración simple estará compuesta por un sintagma nominal y un sintagma verbal (a modo de sujeto y predicado) o solamente por un sintagma verbal (predicado) 
 oracion_simple(os(GN, GV)) --> grupo_nominal(GN), grupo_verbal(GV).
 oracion_simple(os(GV)) --> grupo_verbal(GV).
 
+% Una oración coordinada son dos oraciones simples unidas por una conjuncion.
 oracion_coordinada(oc(OSP,C,OSS)) --> oracion_simple(OSP), conjuncion(C), oracion_simple(OSS).
-%%oracion_coordinada(oc(OS,C,OC)) --> oracion_simple(OS), conjuncion(C), oracion_coordinada(OC).
 
-oracion_subordinada_rel(or(ProRel, OR)) -->
-  pronombre(ProRel),
-  oracion_simple(OR).
+%Una oración subordinada de relativo siempre va precedida por el pronombre relativo que y por una estructura de oración simple (definida previamente)
+oracion_subordinada_rel(or(ProRel, OR)) --> pronombre(ProRel), oracion_simple(OR).
 
+%Una oración se considera compuesta cuando aparece al menos una oración subordinada de relativo o una coordinada
+%Una oración compuesta puede ser una simple oración coordinada
 oracion_compuesta(ocm(OC)) --> oracion_coordinada(OC).
+%Una oración compuesta puede ser una oración coordinada seguida de una conjunción y otra oración compuesta
 oracion_compuesta(ocm(OCD, C, OCM)) --> oracion_coordinada(OCD), conjuncion(C), oracion_compuesta(OCM).
+%Una oración compuesta puede ser una oración simple seguida de una conjunción y otra oración compuesta
 oracion_compuesta(ocm(OS, C, OC)) --> oracion_simple(OS), conjuncion(C), oracion_coordinada(OC).
+%Una oración compuesta puede ser un grupo nominal (con una oración subordinada dentro del mismo) seguido de un grupo verbal
 oracion_compuesta(ocm(GN, GV)) --> grupo_nominal_compuesto(GN), grupo_verbal(GV).
+%La oración subordinada de relativo también puede estar en el grupo verbal el cual puede tener un sujeto (grupo nominal) o no
 oracion_compuesta(ocm(GN, GV)) --> grupo_nominal(GN),  grupo_verbal_compuesto(GV).
 oracion_compuesta(ocm(GV)) --> grupo_verbal_compuesto(GV).
+%Las oraciones subordinadas también pueden aparecer tanto en el grupo nominal como en el verbal
+oracion_compuesta(ocm(GN, GV)) --> grupo_nominal_compuesto(GN),  grupo_verbal_compuesto(GV).
 
-analizar(L, X) :- quitarComas(L,NL), oracion(X, NL, []), write('Descomposicion de la oracion'), nl.% descomponer(X).
 
+%---PREDICADOS AUXILIARES---
+%Predicado que devuelve la oración introducida sin las comas ya que no nos hacen falta para estudiar la estructura de la oración según nuestra definición gramatical
+%Caso base
 quitarComas([],[]).
+%En el caso de que la cabeza sea una coma devolvemos la cola
 quitarComas([X|Y], NL):- 
   X = ',',
   quitarComas(Y,NL).
-
+%En el caso de que la cabeza no sea una coma devolvemos una lista fromada por esa cabeza y la cola con las comas quitadas
 quitarComas([X|Y],NL):-
   not(X = ','),
   quitarComas(Y,NL2),
   NL = [X|NL2].
+
+%Predicado al que se debe llamar para hacer todas las tareas definidas (análisis, descomposición y traducción de la oración)
+analizar(L, X) :- quitarComas(L,NL), oracion(X, NL, []), traducir(eng, X), nl, descomponer(X).
+
--- traductor.pl
+++ traductor.pl
@@ -1,17 +1,7 @@
 :- ['draw.pl'].
 :- ['diccionario_eng.pl']. 
-/*
 
-Traductor b�sico
-
-Ejemplo de funcionamiento:
-
-?- oracion(eng,X,[the,man,eats],[]), oracion(esp,X,Y,[]).
-X = o(gn(det(el), n(hombre)), gv()),
-Y = [el, hombre, come]
-
-*/
-
+%Definición de los símbolos terminales (tanto en español como en inglés)
 nombre_propio(esp, np(X)) --> [X],{np(X, _)}.
 nombre_propio(eng, np(X)) --> [Y],{np(X, Y)}.
 nombre(esp, n(X)) --> [X],{n(X, _)}.
@@ -34,7 +24,7 @@ infinitivo(esp, inf(X)) --> [X],{inf(X, _)}.
 infinitivo(eng, inf(X)) --> [Y],{inf(X, Y)}.
 
 
-%Sintagma Nominal
+%Sintagma Nominal en español
 grupo_nominal(esp, gn(N)) --> nombre_propio(esp, N).
 grupo_nominal(esp, gn(NA,C,NB)) --> nombre_propio(esp, NA), conjuncion(esp, C), nombre_propio(esp, NB).
 grupo_nominal(esp, gn(N)) --> nombre(esp, N).
@@ -44,6 +34,7 @@ grupo_nominal(esp, gn(D,N,GA)) --> determinante(esp, D), nombre(esp, N), grupo_a
 grupo_nominal(esp, gn(D,N,GP)) --> determinante(esp, D), nombre(esp, N), grupo_preposicional(esp, GP).
 grupo_nominal(esp, gn(I,GN)) --> infinitivo(esp, I), grupo_nominal(esp, GN).
 
+%Sintagma Nominal en inglés
 grupo_nominal(eng, gn(N)) --> nombre_propio(eng, N).
 grupo_nominal(eng, gn(NA,C,NB)) --> nombre_propio(eng, NA), conjuncion(eng, C), nombre_propio(eng, NB).
 grupo_nominal(eng, gn(N)) --> nombre(eng, N).
@@ -53,25 +44,29 @@ grupo_nominal(eng, gn(D,N,GA)) --> determinante(eng, D), grupo_adjetival(eng, GA
 grupo_nominal(eng, gn(D,N,GP)) --> determinante(eng, D), nombre(eng, N), grupo_preposicional(eng, GP).
 grupo_nominal(eng, gn(I,GN)) --> infinitivo(eng, I), grupo_nominal(eng, GN).
 
+%Sintagma Nominal Compuesto (SN con oración subordinada) en español
 grupo_nominal_compuesto(esp, gn(GN, OSR)) --> grupo_nominal(esp, GN), oracion_subordinada_rel(esp, OSR).
 
+%Sintagma Nominal Compuesto (SN con oración subordinada) en inglés
 grupo_nominal_compuesto(eng, gn(GN, OSR)) --> grupo_nominal(eng, GN), oracion_subordinada_rel(eng, OSR).
 
-% Sintagma preposicional
+% Sintagma preposicional en español
 grupo_preposicional(esp, gp(P,GN)) --> preposicion(esp, P), grupo_nominal(esp, GN).
+
+% Sintagma preposicional en inglés
 grupo_preposicional(eng, gp(P,GN)) --> preposicion(eng, P), grupo_nominal(eng, GN).
 
-% Sintagma adjatival
+% Sintagma adjetival en español
 grupo_adjetival(esp, gadj(A)) --> adjetivo(esp, A).
 grupo_adjetival(esp, gadj(AV,A)) --> adverbio(esp, AV), adjetivo(esp, A).
 grupo_adjetival(esp, gadj(A,C,GA)) --> adjetivo(esp, A), conjuncion(esp, C), grupo_adjetival(esp, GA).
 
+% Sintagma adjetival en inglés
 grupo_adjetival(eng, gadj(A)) --> adjetivo(eng, A).
 grupo_adjetival(eng, gadj(AV,A)) --> adverbio(eng, AV), adjetivo(eng, A).
 grupo_adjetival(eng, gadj(A,C,GA)) --> adjetivo(eng, A), conjuncion(eng, C), grupo_adjetival(eng, GA).
 
-% Sintagma verbal
-
+% Sintagma verbal en español
 grupo_verbal(esp, gv(VA,C,VB)) --> verbo(esp, VA), conjuncion(esp, C), verbo(esp, VB).
 grupo_verbal(esp, gv(V,GN)) --> verbo(esp, V), grupo_nominal(esp, GN).
 grupo_verbal(esp, gv(V,GN)) --> verbo(esp, V), grupo_nominal_compuesto(esp, GN).
@@ -84,6 +79,7 @@ grupo_verbal(esp, gv(AV,GV)) --> adverbio(esp, AV), grupo_verbal(esp, GV).
 grupo_verbal(esp, gv(V,GA,GN)) --> verbo(esp, V), grupo_adverbial(esp, GA), grupo_nominal(esp, GN).
 grupo_verbal(esp, gv(V)) --> verbo(esp, V).
 
+% Sintagma verbal en inglés
 grupo_verbal(eng, gv(VA,C,VB)) --> verbo(eng, VA), conjuncion(eng, C), verbo(eng, VB).
 grupo_verbal(eng, gv(V,GN)) --> verbo(eng, V), grupo_nominal(eng, GN).
 grupo_verbal(eng, gv(V,GN)) --> verbo(eng, V), grupo_nominal_compuesto(eng, GN).
@@ -96,57 +92,69 @@ grupo_verbal(eng, gv(AV,GV)) --> adverbio(eng, AV), grupo_verbal(eng, GV).
 grupo_verbal(eng, gv(V,GA,GN)) --> verbo(eng, V), grupo_adverbial(eng, GA), grupo_nominal(eng, GN).
 grupo_verbal(eng, gv(V)) --> verbo(eng, V).
 
+%Sintagma Verbal Compuesto (SV con oración subordinada) en español
 grupo_verbal_compuesto(esp, gv(GV, OSR)) --> grupo_verbal(esp, GV), oracion_subordinada_rel(esp, OSR).
 
+%Sintagma Verbal Compuesto (SV con oración subordinada) en inglés
 grupo_verbal_compuesto(eng, gv(GV, OSR)) --> grupo_verbal(eng, GV), oracion_subordinada_rel(eng, OSR).
 
-
-% Sintagma adverbial 
+% Sintagma adverbial en español
 grupo_adverbial(esp, gadv(A)) --> adverbio(esp, A).
 
+% Sintagma adverbial en inglés
 grupo_adverbial(eng, gadv(A)) --> adverbio(eng, A).
 
-% Oraciones 
+% Oraciones (simples y compuestas) en español
 oracion(esp, X, L, []):- oracion_simple(esp, X, L, []), !.
 oracion(esp, X, L, []):- oracion_compuesta(esp, X, L, []), !.
 
+% Oraciones (simples y compuestas) en inglés
 oracion(eng, X, L, []):- oracion_simple(eng, X, L, []), !.
 oracion(eng, X, L, []):- oracion_compuesta(eng, X, L, []), !.
 
-
+% Oraciones simples en español
 oracion_simple(esp, os(GN, GV)) --> grupo_nominal(esp, GN), grupo_verbal(esp, GV).
 oracion_simple(esp, os(GV)) --> grupo_verbal(esp, GV).
 
+% Oraciones simples en inglés
 oracion_simple(eng, os(GN, GV)) --> grupo_nominal(eng, GN), grupo_verbal(eng, GV).
 oracion_simple(eng, os(GV)) --> grupo_verbal(eng, GV).
 
+% Oracion coordinada en español
 oracion_coordinada(esp, oc(OSP,C,OSS)) --> oracion_simple(esp, OSP), conjuncion(esp, C), oracion_simple(esp, OSS).
-%%oracion_coordinada(oc(OS,C,OC)) --> oracion_simple(OS), conjuncion(C), oracion_coordinada(OC).
+
+% Oracion coordinada en inglés
 oracion_coordinada(eng, oc(OSP,C,OSS)) --> oracion_simple(eng, OSP), conjuncion(eng, C), oracion_simple(eng, OSS).
 
-oracion_subordinada_rel(esp, or(ProRel, OR)) -->
-  pronombre(esp, ProRel),
-  oracion_simple(esp, OR).
+% Oracion subordinada en español
+oracion_subordinada_rel(esp, or(ProRel, OR)) --> pronombre(esp, ProRel), oracion_simple(esp, OR).
 
-  oracion_subordinada_rel(eng, or(ProRel, OR)) -->
-  pronombre(eng, ProRel),
-  oracion_simple(eng, OR).
+% Oracion subordinada en inglés
+oracion_subordinada_rel(eng, or(ProRel, OR)) --> pronombre(eng, ProRel), oracion_simple(eng, OR).
 
+% Oracion compuesta en español
 oracion_compuesta(esp, ocm(OC)) --> oracion_coordinada(esp, OC).
 oracion_compuesta(esp, ocm(OCD, C, OCM)) --> oracion_coordinada(esp, OCD), conjuncion(esp, C), oracion_compuesta(esp, OCM).
 oracion_compuesta(esp, ocm(OS, C, OC)) --> oracion_simple(esp, OS), conjuncion(esp, C), oracion_coordinada(esp, OC).
 oracion_compuesta(esp, ocm(GN, GV)) --> grupo_nominal_compuesto(esp, GN), grupo_verbal(esp, GV).
 oracion_compuesta(esp, ocm(GN, GV)) --> grupo_nominal(esp, GN),  grupo_verbal_compuesto(esp, GV).
 oracion_compuesta(esp, ocm(GV)) --> grupo_verbal_compuesto(esp, GV).
+oracion_compuesta(esp, ocm(GN, GV)) --> grupo_nominal_compuesto(esp, GN),  grupo_verbal_compuesto(esp, GV).
 
+% Oracion compuesta en inglés
 oracion_compuesta(eng, ocm(OC)) --> oracion_coordinada(eng, OC).
 oracion_compuesta(eng, ocm(OCD, C, OCM)) --> oracion_coordinada(eng, OCD), conjuncion(eng, C), oracion_compuesta(eng, OCM).
 oracion_compuesta(eng, ocm(OS, C, OC)) --> oracion_simple(eng, OS), conjuncion(eng, C), oracion_coordinada(eng, OC).
 oracion_compuesta(eng, ocm(GN, GV)) --> grupo_nominal_compuesto(eng, GN), grupo_verbal(eng, GV).
 oracion_compuesta(eng, ocm(GN, GV)) --> grupo_nominal(eng, GN),  grupo_verbal_compuesto(eng, GV).
 oracion_compuesta(eng, ocm(GV)) --> grupo_verbal_compuesto(eng, GV).
-%-----------------------------------------------------------------------------------------------------------------------
+oracion_compuesta(eng, ocm(GN, GV)) --> grupo_nominal_compuesto(eng, GN),  grupo_verbal_compuesto(eng, GV).
 
+%---PREDICADOS AUXILIARES---
+% Predicado que dada la lista que contiene las palabras de la frase traducida, devuelve la frase traducida
 escribir([]).
 escribir([X|Y]):-write(X), write(' '),escribir(Y).
+
+% Predicado que, dada la estructura de la frase en español, devuelve la frase traducida en inglés
+traducir(eng, X):- oracion(eng, X, Y, []), write('Traducción al inglés'),nl, escribir(Y), !.
 
